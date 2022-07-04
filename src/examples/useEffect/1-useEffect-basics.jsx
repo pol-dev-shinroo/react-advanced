d68eafc600af1d53c722f5bc04996eb48552a294
Paul Kim <manus.dream.language@gmail.com>
2022-07-04 18:03:30 +0900

    useEffect cannot be called conditionally

--- src/examples/useEffect/1-useEffect-basics.jsx
+++ src/examples/useEffect/1-useEffect-basics.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 
 const Basics = () => {
+    const [value, setValue] = useState(0);
+    // useEffect hook cannot be called conditionally!!!
+    if (value > 0) {
+        useEffect(() => {
+            document.title = `New ${value}`;
+        });
+    }
+
+    // instead:
     useEffect(() => {
-        document.title = `New ${value}`;
+        if (value > 0) {
+            document.title = `New ${value}`;
+        }
     });
 
-    const [value, setValue] = useState(0);
     const valueHandler = () => {
         setValue((prev) => {
             return prev + 1;
         });
-        document.title = `New (${value})`;
     };
     return (
         <div>
